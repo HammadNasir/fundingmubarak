55fe996e70bba182e42aa1836f62579288ed5273
Hammad Nasir <hns.billionaires@gmail.com>
2022-02-01 23:18:12 +0530

    initial commit

---
+++ bot.js
@@ -0,0 +1,31 @@
+const { auth, postReplyWithMedia, postReply } = require("./config.js");
+
+const client = auth();
+
+client.stream("statuses/filter", { track: "#fundraise" }, function (stream) {
+  console.log("Searching for tweets...");
+
+  // when a tweet is found
+  stream.on("data", function (tweet) {
+    console.log("Found one!");
+    console.log("Recieved tweet reading...", tweet.text);
+
+    // check if tweet contains some media
+    // if (tweet.media_url) {
+    //   console.log("Replying to tweet with video.");
+    postReplyWithMedia(
+      client,
+      "./Kya-karenge-aap-iss-dhan-rashi-ka-KBC-memes.jpeg",
+      tweet
+    );
+    // } else {
+    //   console.log("Tweet didn't provide media. Replying with message.");
+    // const message = "Voila!";
+    // postReply(client, message, tweet);
+    // }
+
+    stream.on("error", function (error) {
+      console.log(error);
+    });
+  });
+});
